d9d2a4d17a57a6a7e5019f8165e2b58181bb1fbc
fb0801 <fbhatti0801@gmail.com>
2023-09-04 19:19:31 +0100

    collision for car insurance

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 FINISH = pygame.image.load('imgs/finish.png')
 
 RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
@@ -53,11 +54,24 @@ class AbstractCar:
         self.y -= vertical
         self.x -= horizontal
 
-    
+    def collide(self, mask, x=0,y=0):
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x - x), int(self.y - y))
+        poi = mask.overlap(car_mask, offset)
+        return poi
 
 class PlayerCar(AbstractCar):
     IMG = RED_CAR
     START_POS = (180, 200)
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel 
+        self.move()
+
 
 def draw(win, images, player_car):
     for img, pos in images:
@@ -66,9 +80,7 @@ def draw(win, images, player_car):
     player_car.draw(win)
     pygame.display.update()
 
-def reduce_speed(self):
-        self.vel = max(self.vel - self.acceleration / 2, 0)
-        self.move()
+
 
 
 run = True
@@ -106,5 +118,7 @@ while run:
             break
 
     move_player(player_car)
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()    
 
 pygame.quit()
