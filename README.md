3ab5c300a5ae3b58cf06a966cfb37e5e300967da
fb0801 <fbhatti0801@gmail.com>
2023-09-04 15:41:36 +0100

    display imgs

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # python-car-game
-Car game built in python using pygame
+Car game built in python using pygame video by <a href="https://www.youtube.com/watch?v=L3ktUWfAMPg">techwithTim</a>
 
 
-#Issues:
+## Issues:
 
+## Changes made:
+
+
+## Resources used:
+
+https://www.youtube.com/watch?v=L3ktUWfAMPg
+
+original repo -> https://github.com/techwithtim/Pygame-Car-Racer/tree/main
+
+https://www.toptal.com/developers/gitignore/
--- main.py
+++ main.py
@@ -15,3 +15,24 @@ GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
+
+FPS = 60
+
+run = True
+clock = pygame.time.Clock()
+while run:
+    clock.tick(FPS)
+
+    WIN.blit(GRASS, (0,0))
+    WIN.blit(TRACK, (0,0))
+    WIN.blit(FINISH, (0,0))
+
+
+
+    pygame.display.update()
+
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            run = False
+            break
+pygame.quit()
