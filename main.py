47b916b103f204814f9acbf0d7a351f9866b246a
fb0801 <fbhatti0801@gmail.com>
2023-09-05 19:51:45 +0100

    path following

--- main.py
+++ main.py
@@ -102,6 +102,41 @@ class ComputerCar(AbstractCar):
         super().draw(win)
         self.draw_points(win)
 
+    def calculate_angle(self):
+        target_x, target_y = self.path[self.current_point]
+        x_diff = target_x - self.x
+        y_diff = target_y - self.y
+
+        if y_diff == 0:
+            desired_radian_angle = math.pi / 2
+        else:
+            desired_radian_angle = math.atan(x_diff/y_diff)
+
+        if target_y > self.y:
+            desired_radian_angle += math.pi
+
+        difference_in_angle = self.angle - math.degrees(desired_radian_angle)
+        if difference_in_angle >=100:
+            difference_in_angle -= 360
+
+        if difference_in_angle > 0:
+            self.angle -= min(self.rotation_vel, abs(difference_in_angle))
+        else:                        
+            self.angle += min(self.rotation_vel, abs(difference_in_angle))
+
+    def update_path_point(self):
+        target = self.path[self.current_point]
+        rect = pygame.Rect(self.x, self.y, self.img.get_width(), self.img.get_height())
+        if rect.collidepoint(*target):
+            self.current_point += 1
+
+    def move(self):
+        if self.current_point >= len(self.path):
+            return 
+        self.calculate_angle()
+        self.update_path_point()
+        super().move()
+
 
 
 def draw(win, images, player_car, computer_car):
@@ -153,6 +188,7 @@ while run:
         
 
     move_player(player_car)
+    computer_car.move()
 
     if player_car.collide(TRACK_BORDER_MASK) != None:
         player_car.bounce()    
